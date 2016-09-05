142916e8ba860e2b9c4badb5068f16a09cb50f60
Alessandro Pieropan <CoffeRobot@users.noreply.github.com>
2016-09-05 11:38:36 +0900

    removed dependency from SOIL, using opencv to load textures

--- include/mesh.h
+++ include/mesh.h
@@ -47,6 +47,7 @@ using namespace std;
 
 #include "shader.h"
 
+namespace rendering{
 
 struct Vertex {
     // Position
@@ -101,13 +102,13 @@ public:
                 ss << specularNr++; // Transfer GLuint to stream
             number = ss.str();
             // Now set the sampler to the correct texture unit
-            glUniform1i(glGetUniformLocation(shader.Program, (name + number).c_str()), i);
+            glUniform1i(glGetUniformLocation(shader.program_id_, (name + number).c_str()), i);
             // And finally bind the texture
             glBindTexture(GL_TEXTURE_2D, this->textures[i].id);
         }
 
         // Also set each mesh's shininess property to a default value (if you want you could extend this to another mesh property and possibly change this value)
-        glUniform1f(glGetUniformLocation(shader.Program, "material.shininess"), 16.0f);
+        glUniform1f(glGetUniformLocation(shader.program_id_, "material.shininess"), 16.0f);
 
         // Draw mesh
         glBindVertexArray(this->VAO);
@@ -162,5 +163,7 @@ private:
 };
 
 
+
+} // end namespace
 
 #endif // MESH_H
--- src/opengl_integration.cpp
+++ src/opengl_integration.cpp
@@ -51,13 +51,20 @@
 #include <glm/gtc/type_ptr.hpp>
 
 // Other Libs
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
 //#include <SOIL.h>
+#include <iostream>
+
 
 // Properties
 GLuint screenWidth = 800, screenHeight = 600;
 
 // Function prototypes
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void key_callback(GLFWwindow* window, int key, int scancode, int action,
+                  int mode);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void Do_Movement();
@@ -72,167 +79,202 @@ GLfloat deltaTime = 0.0f;
 GLfloat lastFrame = 0.0f;
 
 // The MAIN function, from here we start our application and run our Game loop
-int main()
-{
-    // Init GLFW
-    glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+int main() {
+  // Init GLFW
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
-    GLFWwindow* window = glfwCreateWindow(screenWidth, screenHeight, "LearnOpenGL", nullptr, nullptr); // Windowed
-    glfwMakeContextCurrent(window);
+  GLFWwindow* window = glfwCreateWindow(
+      screenWidth, screenHeight, "LearnOpenGL", nullptr, nullptr);  // Windowed
+  glfwMakeContextCurrent(window);
 
-    // Set the required callback functions
-    glfwSetKeyCallback(window, key_callback);
-    glfwSetCursorPosCallback(window, mouse_callback);
-    glfwSetScrollCallback(window, scroll_callback);
+  // Set the required callback functions
+  glfwSetKeyCallback(window, key_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
+  glfwSetScrollCallback(window, scroll_callback);
 
-    // Options
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  // Options
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 
-    // Initialize GLEW to setup the OpenGL Function pointers
-    glewExperimental = GL_TRUE;
-    glewInit();
+  // Initialize GLEW to setup the OpenGL Function pointers
+  glewExperimental = GL_TRUE;
+  glewInit();
 
-    // Define the viewport dimensions
-    glViewport(0, 0, screenWidth, screenHeight);
+  // Define the viewport dimensions
+  glViewport(0, 0, screenWidth, screenHeight);
 
-    // Setup some OpenGL options
-    glEnable(GL_DEPTH_TEST);
+  // Setup some OpenGL options
+  glEnable(GL_DEPTH_TEST);
 
-    // Setup and compile our shaders
-    Shader shader("/home/alessandro/Projects/rendering_engine/data/simple.vs", "/home/alessandro/Projects/rendering_engine/data/simple.frag");
+  // Setup and compile our shaders
+  rendering::Shader shader(
+      "/home/alessandro/projects/rendering_engine/data/shaders/simple.vs",
+      "/home/alessandro/projects/rendering_engine/data/shaders/simple.frag");
 
-    // Load models
-    //Model ourModel("/home/alessandro/projects/drone_ws/src/fato_tracker/data/ros_hydro/ros_hydro.obj");
+  // Load models
+  // Model
+  // ourModel("/home/alessandro/projects/drone_ws/src/fato_tracker/data/ros_hydro/ros_hydro.obj");
 
-    // Draw in wireframe
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+  // Draw in wireframe
+  // glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-    GLfloat vertices[] = {
-        -0.5f, -0.5f, 0.0f,
-         0.5f, -0.5f, 0.0f,
-         0.0f,  0.5f, 0.0f
-    };
+  GLfloat vertices[] = {
+      // Positions          // Colors           // Texture Coords
+      0.5f,  0.5f,  0.0f, 1.0f, 0.0f,
+      0.0f,  1.0f,  1.0f,  // Top Right
+      0.5f,  -0.5f, 0.0f, 0.0f, 1.0f,
+      0.0f,  1.0f,  0.0f,  // Bottom Right
+      -0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+      1.0f,  0.0f,  0.0f,  // Bottom Left
+      -0.5f, 0.5f,  0.0f, 1.0f, 1.0f,
+      0.0f,  0.0f,  1.0f  // Top Left
+  };
+  GLuint indices[] = {  // Note that we start from 0!
+          0, 1, 3, // First Triangle
+          1, 2, 3  // Second Triangle
+      };
 
-    GLuint VBO, VAO;
-    glGenVertexArrays(1, &VAO);
-    glGenBuffers(1, &VBO);
-    // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and attribute pointer(s).
+  GLuint VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+  // Bind the Vertex Array Object first, then bind and set vertex buffer(s) and
+  // attribute pointer(s).
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices,
+               GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+                        (GLvoid*)0);
+  glEnableVertexAttribArray(0);
+  // Color attribute
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+                        (GLvoid*)(3 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(1);
+
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(GLfloat),
+                        (GLvoid*)(6 * sizeof(GLfloat)));
+  glEnableVertexAttribArray(2);
+
+
+  glBindVertexArray(0);  // Unbind VAO (it's always a good thing to unbind any
+                         // buffer/array to prevent strange bugs)
+
+  // Load and create a texture
+  GLuint texture;
+  glGenTextures(1, &texture);
+  glBindTexture(GL_TEXTURE_2D, texture);  // All upcoming GL_TEXTURE_2D
+                                          // operations now have effect on this
+                                          // texture object
+  // Set the texture wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S,
+                  GL_REPEAT);  // Set texture wrapping to GL_REPEAT (usually
+                               // basic wrapping method)
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // Set texture filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // Load image, create texture and generate mipmaps
+
+  cv::Mat rgb_img = cv::imread("/home/alessandro/projects/rendering_engine/data/textures/container.jpg");
+  cv::cvtColor(rgb_img, rgb_img, CV_BGR2RGB);
+  //unsigned char* image = SOIL_load_image(
+  //    "/home/alessandro/projects/rendering_engine/data/textures/container.jpg",
+  //    &width, &height, 0, SOIL_LOAD_RGB);
+  unsigned char* img_data = rgb_img.data;
+
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, rgb_img.cols, rgb_img.rows, 0, GL_RGB,
+               GL_UNSIGNED_BYTE, img_data);
+  glGenerateMipmap(GL_TEXTURE_2D);
+  //SOIL_free_image_data(image);
+  glBindTexture(GL_TEXTURE_2D, 0);  // Unbind texture when done, so we won't
+
+
+  // accidentily mess up our texture.
+
+  // Game loop
+  while (!glfwWindowShouldClose(window)) {
+    // Set frame time
+    GLfloat currentFrame = glfwGetTime();
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+
+    // Check and call events
+    glfwPollEvents();
+    // Do_Movement();
+
+    // Clear the colorbuffer
+    glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Bind Texture
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    shader.use();  // <-- Don't forget this one!
+
+    // Draw our first triangle
     glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+    glBindVertexArray(0);
 
-    glBindBuffer(GL_ARRAY_BUFFER, VBO);
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // Swap the buffers
+    glfwSwapBuffers(window);
+  }
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(GLfloat), (GLvoid*)0);
-    glEnableVertexAttribArray(0);
-
-    glBindBuffer(GL_ARRAY_BUFFER, 0); // Note that this is allowed, the call to glVertexAttribPointer registered VBO as the currently bound vertex buffer object so afterwards we can safely unbind
-
-    glBindVertexArray(0); // Unbind VAO (it's always a good thing to unbind any buffer/array to prevent strange bugs)
-
-    // Game loop
-    while(!glfwWindowShouldClose(window))
-    {
-        // Set frame time
-        GLfloat currentFrame = glfwGetTime();
-        deltaTime = currentFrame - lastFrame;
-        lastFrame = currentFrame;
-
-        // Check and call events
-        glfwPollEvents();
-        //Do_Movement();
-
-        // Clear the colorbuffer
-        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        shader.Use();   // <-- Don't forget this one!
-
-        // Draw our first triangle
-        glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 3);
-        glBindVertexArray(0);
-
-        // Swap the screen buffers
-        glfwSwapBuffers(window);
-
-//        // Transformation matrices
-//        glm::mat4 projection = glm::perspective(camera.Zoom, (float)screenWidth/(float)screenHeight, 0.1f, 100.0f);
-//        glm::mat4 view = camera.GetViewMatrix();
-//        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "projection"), 1, GL_FALSE, glm::value_ptr(projection));
-//        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "view"), 1, GL_FALSE, glm::value_ptr(view));
-
-//        // Draw the loaded model
-//        glm::mat4 model;
-//        model = glm::translate(model, glm::vec3(0.0f, -1.75f, 0.0f)); // Translate it down a bit so it's at the center of the scene
-//        model = glm::scale(model, glm::vec3(0.2f, 0.2f, 0.2f));	// It's a bit too big for our scene, so scale it down
-//        glUniformMatrix4fv(glGetUniformLocation(shader.Program, "model"), 1, GL_FALSE, glm::value_ptr(model));
-//        ourModel.Draw(shader);
-
-        // Swap the buffers
-        glfwSwapBuffers(window);
-    }
-
-    glDeleteVertexArrays(1, &VAO);
-    glDeleteBuffers(1, &VBO);
-    // Terminate GLFW, clearing any resources allocated by GLFW.
-    glfwTerminate();
-    return 0;
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+  // Terminate GLFW, clearing any resources allocated by GLFW.
+  glfwTerminate();
+  return 0;
 }
 
-
 // Moves/alters the camera positions based on user input
-void Do_Movement()
-{
-    // Camera controls
-    if(keys[GLFW_KEY_W])
-        camera.ProcessKeyboard(FORWARD, deltaTime);
-    if(keys[GLFW_KEY_S])
-        camera.ProcessKeyboard(BACKWARD, deltaTime);
-    if(keys[GLFW_KEY_A])
-        camera.ProcessKeyboard(LEFT, deltaTime);
-    if(keys[GLFW_KEY_D])
-        camera.ProcessKeyboard(RIGHT, deltaTime);
+void Do_Movement() {
+  // Camera controls
+  if (keys[GLFW_KEY_W]) camera.ProcessKeyboard(FORWARD, deltaTime);
+  if (keys[GLFW_KEY_S]) camera.ProcessKeyboard(BACKWARD, deltaTime);
+  if (keys[GLFW_KEY_A]) camera.ProcessKeyboard(LEFT, deltaTime);
+  if (keys[GLFW_KEY_D]) camera.ProcessKeyboard(RIGHT, deltaTime);
 }
 
 // Is called whenever a key is pressed/released via GLFW
-void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode)
-{
-    if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, GL_TRUE);
+void key_callback(GLFWwindow* window, int key, int scancode, int action,
+                  int mode) {
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, GL_TRUE);
 
-    if(action == GLFW_PRESS)
-        keys[key] = true;
-    else if(action == GLFW_RELEASE)
-        keys[key] = false;
+  if (action == GLFW_PRESS)
+    keys[key] = true;
+  else if (action == GLFW_RELEASE)
+    keys[key] = false;
 }
 
-void mouse_callback(GLFWwindow* window, double xpos, double ypos)
-{
-    if(firstMouse)
-    {
-        lastX = xpos;
-        lastY = ypos;
-        firstMouse = false;
-    }
-
-    GLfloat xoffset = xpos - lastX;
-    GLfloat yoffset = lastY - ypos;
-
+void mouse_callback(GLFWwindow* window, double xpos, double ypos) {
+  if (firstMouse) {
     lastX = xpos;
     lastY = ypos;
+    firstMouse = false;
+  }
 
-    camera.ProcessMouseMovement(xoffset, yoffset);
+  GLfloat xoffset = xpos - lastX;
+  GLfloat yoffset = lastY - ypos;
+
+  lastX = xpos;
+  lastY = ypos;
+
+  camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
-void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
-{
-    camera.ProcessMouseScroll(yoffset);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset) {
+  camera.ProcessMouseScroll(yoffset);
 }
-
-
-
-
